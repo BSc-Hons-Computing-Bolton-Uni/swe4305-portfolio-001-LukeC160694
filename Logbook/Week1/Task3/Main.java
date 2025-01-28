723c583b28985a15de8829986d6356ede2f0490e
LukeC160694 <100214045@student.burnley.ac.uk>
2025-01-28 12:49:45 +0000

    Updated fix

--- Logbook/Week1/Task3/Main.java
+++ Logbook/Week1/Task3/Main.java
@@ -1,1 +1,20 @@
 package Logbook.Week1.Task3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the height of the rectangle: ");
+        double height = scanner.nextDouble();
+
+        double perimeter = 2 * (length + height);
+        double area = length * height;
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+        scanner.close();
+    }
+}
