df712e0e10f93c6f67e334feb9a3c8d4f113d1ef
LukeC160694 <100214045@student.burnley.ac.uk>
2025-01-28 14:25:12 +0000

    Updated fix

--- Logbook/Week1/Task4/AverageCalculator.java
+++ Logbook/Week1/Task4/AverageCalculator.java
@@ -1,1 +1,16 @@
 package Logbook.Week1.Task4;
+import java.util.Scanner;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double average = (num1 + num2) / 2;
+        System.out.println("The average is: " + average);
+        scanner.close();
+    }
+}
