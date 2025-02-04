5826afba521c5821d9165e17254b592f90472d74
LukeC160694 <100214045@student.burnley.ac.uk>
2025-02-04 14:50:28 +0000

    Updated fix

--- Logbook/Week2/main.java
+++ Logbook/Week2/main.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        //  Program 1: Age Check
+        /**
+         * Program 1: Age check
+         * gets the user to input their age and outputs their birth year
+         * as well if they are an adult or under 18.
+          */
         System.out.println("Program 1: Age Check");
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
@@ -20,7 +23,11 @@ public class main {
 
         System.out.println("\n-------------------------");
 
-        // Program 2: Grade Classification
+        /**
+         * Program 2: Grade classification
+         * Asks the user for their letter grade
+         * And outputs the result as a university classification.
+         */
         scanner.nextLine();
         System.out.println("Program 2: Grade Classification");
         System.out.print("Enter your letter grade (A, B, C, D, E, F): ");
@@ -50,14 +57,20 @@ public class main {
 
         System.out.println("\n-------------------------");
 
-        // Program 3: Print Digits 0-9
+        /**
+         * Program 3: Print digits 0-9
+         * prints out the digit from numbers 0 to 9 for the user.
+         */
         System.out.println("Program 3: Digits from 0 to 9");
         for (int i = 0; i <= 9; i++) {
             System.out.print(i + " ");
         }
         System.out.println("\n-------------------------");
 
-        // Program 4: 7-Times Table
+        /**
+         * Program 4: 7-times table
+         * Outputs the 7 times table to the user.
+         */
         System.out.println("Program 4: 7-Times Table");
         for (int i = 1; i <= 12; i++) {
             System.out.println(i + " x 7 = " + (i * 7));
@@ -65,7 +78,11 @@ public class main {
 
         System.out.println("\n-------------------------");
 
-        //  Program 5: Custom Multiplication Table
+        /**
+         * Program 5: Custom multiplication table
+         * Asks the user to input a number from 1 to 12
+         * and outputs that numbers times table.
+         */
         System.out.println("Program 5: Custom Multiplication Table");
         System.out.print("Enter a number to see its multiplication table: ");
         int number = scanner.nextInt();
@@ -75,7 +92,11 @@ public class main {
 
         System.out.println("\n-------------------------");
 
-        // Program 6: Multiplication Table with Restart
+        /**
+         * Program 6: Multiplication table with restart
+         * The same as the previous program except with an option after each times table set
+         * to restart and begin again with a new number or end the program.
+         */
         char choice;
         do {
             System.out.println("Program 6: Multiplication Table with Restart");
@@ -91,7 +112,12 @@ public class main {
 
         System.out.println("\n-------------------------");
 
-        // Program 7: Exam Mark to Grade
+        /**
+         * Program 7: Exam mark to grade
+         * Asks the user to enter their grade value from 0 to 100
+         * and proceeds to return the university grade that is
+         * associated with that number given.
+         */
         System.out.println("Program 7: Exam Mark to Grade");
         System.out.print("Enter your exam mark (0-100): ");
         int mark = scanner.nextInt();
@@ -109,6 +135,11 @@ public class main {
 
         System.out.println("\n-------------------------");
 
+        /**
+         * Program 8: Print even numbers
+         * A program that will print out the even numbers to
+         * the user between the numbers 0 and 10.
+         */
         //  Program 8: Print Even Numbers
         System.out.println("Program 8: Print Even Numbers");
         for (int i = 0; i < 10; i++) {
